6f01ebeebfafa1c809fda3d6508bba1c4a2b5430
Naykat <nidan120107@gmail.com>
2022-06-15 20:05:22 +0300

    Try to create a BT

---
+++ BT.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class Tree {
+private:
+    Tree*Right = nullptr
+    Tree*Left = nullptr
+    int data;
+public:
+    Beginning (T data) {
+        this->data=data
+        this->Right = nullptr
+        this->Left = nullptr
+    };
+
+    Add (T data) {
+        if (data > //smth) 
+        {
+            //smth->Right = data
+        };
+        else {
+            //smth->Left = data
+        }
+    }
+
+};
